aa53e9cb4fdfa209f3eac05376e6b0cbf3d58e1b
alex <liuhlightning@qq.com>
2026-06-06 14:33:34 +0800

    toolchain

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,8 @@
 import eslint from "@eslint/js";
+import prettier from "eslint-config-prettier";
 import { defineConfig } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
 
 export default defineConfig({
   extends: [
